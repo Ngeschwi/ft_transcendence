4b651062e3ef74a2d19ad67cbf7506dc757782f3
Nathan Geschwind <ngeschwi@z1r8p2.42lyon.fr>
2022-11-23 19:01:03 +0100

    work with 2 players, todo: protect things: enter, disconnect player, ...

--- test/pong_socket/index.js
+++ test/pong_socket/index.js
@@ -15,7 +15,8 @@ app.get('/', function(req, res,next) {
 let nbrPlayer = 0;
 let gameState = 'notStarted';
 let maxPoints = 5;
-let player;
+let player1;
+let player2;
 let ball;
 
 let positionBoard;
@@ -66,8 +67,9 @@ class Player {
 }
 
 class Ball {
-    constructor(position, client) {
-        this.client = client;
+    constructor(position, player1, player2) {
+        this.player1 = player1;
+        this.player2 = player2;
         this.speed = 3;
         this.top = position.top;
         this.left = position.left;
@@ -87,7 +89,8 @@ class Ball {
         this.bottom = this.top + this.height;
         this.centerY = this.top + this.height / 2;
 
-        this.client.emit('moveBall', {top: this.top, left: this.left});
+        this.player1.client.emit('moveBall', {top: this.top, left: this.left});
+        this.player2.client.emit('moveBall', {top: this.top, left: this.left});
     }
 
     resetPlace() {
@@ -99,7 +102,141 @@ class Ball {
         this.directionX = Math.floor(Math.random() * 2) === 0 ? -1 * this.speed : this.speed;
         this.directionY = 0;
 
-        this.client.emit('resetBall', {top: this.top, left: this.left});
+        this.player1.client.emit('resetBall', {top: this.top, left: this.left});
+        this.player2.client.emit('resetBall', {top: this.top, left: this.left});
+    }
+}
+
+function getPoint(whoScore) {
+    if (whoScore === 'left') {
+        player1.score++;
+        player1.client.emit('updateScore', {id: player1.id, score: player1.score});
+        player2.client.emit('updateScore', {id: player1.id, score: player1.score});
+        if (player1.score === maxPoints) {
+            player1.client.emit('win', player1.id);
+            gameState = 'notStarted';
+            return Do.END;
+        }
+    } else{
+        player2.score++;
+        player2.client.emit('updateScore', {id: player2.id, score: player2.score});
+        player1.client.emit('updateScore', {id: player2.id, score: player2.score});
+        if (player2.score === maxPoints) {
+            player2.client.emit('win', player2.id);
+            gameState = 'notStarted';
+            return Do.END;
+        }
+    }
+    ball.resetPlace();
+    return Do.CONTINUE;
+}
+
+function getNewDirection(hit) {
+    if (hit === 'left') {
+        ball.directionY = Math.tan((ball.centerY - player1.centerY) / (player2.height / 2)) * 2;
+        ball.directionX = ball.speed + (Math.abs(ball.directionY) / 2);
+    } else {
+        ball.directionY = Math.tan((ball.centerY - player1.centerY) / (player2.height / 2)) * 2;
+        ball.directionX = -ball.speed - (Math.abs(ball.directionY) / 2);
+    }
+    ball.move();
+    setTimeout(moveBall, 10);
+}
+
+function movePaddle() {
+    if (keyPress['w']) {
+        player1.move(Direction.UP);
+        player2.client.emit('moveOtherPaddle', {top: player1.top, id: player1.id});
+    }
+    if (keyPress['s']) {
+        player1.move(Direction.DOWN);
+        player2.client.emit('moveOtherPaddle', {top: player1.top, id: player1.id});
+    }
+    if (keyPress['ArrowUp']) {
+        player2.move(Direction.UP);
+        player1.client.emit('moveOtherPaddle', {top: player2.top, id: player2.id});
+    }
+    if (keyPress['ArrowDown']) {
+        player2.move(Direction.DOWN);
+        player1.client.emit('moveOtherPaddle', {top: player2.top, id: player2.id});
+    }
+}
+
+function moveBall() {
+    // if the ball touch the left or right of the board
+    if (ball.left <= positionBoard.left) {
+        if (getPoint('right'))
+            return Do.END;
+    }
+    if (ball.right >= positionBoard.right) {
+        if (getPoint('left'))
+            return Do.END;
+    }
+
+    // if the ball touch the left or right paddle
+    if (ball.left <= player1.right
+        && ball.bottom >= player1.top
+        && ball.top <= player1.bottom
+        && ball.left >= player1.left) {
+        getNewDirection('left');
+        return Do.CONTINUE;
+    }
+    if (ball.right >= player2.left
+        && ball.bottom >= player2.top
+        && ball.top <= player2.bottom
+        && ball.right <= player2.right) {
+        getNewDirection('right');
+        return Do.CONTINUE;
+    }
+
+    // if the ball touch the top or bottom of the board
+    if (ball.top <= positionBoard.top) {
+        ball.directionY = -ball.directionY;
+    }
+    if (ball.bottom >= positionBoard.bottom) {
+        ball.directionY = -ball.directionY;
+    }
+
+    // if the ball the top or bottom of the paddle
+    if ((ball.top <= player1.bottom
+        || ball.bottom >= player1.top)
+        && ball.left <= player1.right
+        && ball.right >= player1.left) {
+        ball.directionY = -ball.directionY;
+    }
+    if ((ball.bottom >= player2.top
+        || ball.top <= player2.bottom)
+        && ball.left <= player2.right
+        && ball.right >= player2.left) {
+        ball.directionY = -ball.directionY;
+    }
+
+    ball.move();
+}
+
+function moveAll() {
+    if (moveBall() === Do.END)
+        return ;
+    movePaddle();
+    setTimeout(moveAll, 10);
+}
+
+function resetGame() {
+    console.log('reset');
+    player1.score = 0;
+    player2.score = 0;
+    player1.client.emit('updateScore', {id: player1.id, score: player1.score});
+    player2.client.emit('updateScore', {id: player1.id, score: player1.score});
+    player1.resetPlace();
+    player2.resetPlace();
+    ball.resetPlace();
+}
+
+function startGame() {
+    if (gameState === 'notStarted') {
+        gameState = 'started';
+        resetGame();
+        moveAll();
     }
 }
 
@@ -108,9 +245,7 @@ io.on('connection', function(client) {
     console.log('Client connected...');
 
     client.on('init', function(data) {
-        positionBoard = data.positionBoard;
-        // attention creation de deux balls
-        ball = new Ball(data.ball, client);
+        positionBoard = data;
     });
 
     nbrPlayer++;
@@ -118,7 +253,12 @@ io.on('connection', function(client) {
 
     client.on('player_join', function(data) {
         console.log('Player ' + data.id + ' join');
-        player = new Player(data.position, data.score, data.id, client);
+        if (data.id === 1) {
+            player1 = new Player(data.position, data.score, data.id, client);
+        } else {
+            player2 = new Player(data.position, data.score, data.id, client);
+            ball = new Ball(data.ballPosition, player1, player2);
+        }
     });
 
     // voir pour mieux gerer les touches
@@ -132,131 +272,6 @@ io.on('connection', function(client) {
     client.on('keyUp', function(data) {
         keyPress[data] = false;
     });
-
-    function getPoint(whoScore) {
-        if (whoScore === 'left') {
-            player.score++;
-            client.emit('updateScore', {id: player.id, score: player.score});
-            if (player.score === maxPoints) {
-                client.emit('win', player.id);
-                gameState = 'notStarted';
-                return Do.END;
-            }
-        } else{
-            player.score++;
-            client.emit('updateScore', {id: player.id, score: player.score});
-            if (player.score === maxPoints) {
-                client.emit('win', player.id);
-                gameState = 'notStarted';
-                return Do.END;
-            }
-        }
-        ball.resetPlace();
-        return Do.CONTINUE;
-    }
-
-    function getNewDirection(hit) {
-        if (hit === 'left') {
-            ball.directionY = Math.tan((ball.centerY - player.centerY) / (player.height / 2)) * 2;
-            ball.directionX = ball.speed + (Math.abs(ball.directionY) / 2);
-        } else {
-            ball.directionY = Math.tan((ball.centerY - player.centerY) / (player.height / 2)) * 2;
-            ball.directionX = -ball.speed - (Math.abs(ball.directionY) / 2);
-        }
-        ball.move();
-        setTimeout(moveBall, 10);
-    }
-
-    function movePaddle() {
-        if (keyPress['w']) {
-            player.move(Direction.UP);
-        }
-        if (keyPress['s']) {
-            player.move(Direction.DOWN);
-        }
-        if (keyPress['ArrowUp']) {
-            player.move(Direction.UP);
-        }
-        if (keyPress['ArrowDown']) {
-            player.move(Direction.DOWN);
-        }
-    }
-
-    function moveBall() {
-        // if the ball touch the left or right of the board
-        if (ball.left <= positionBoard.left) {
-            if (getPoint('right'))
-                return Do.END;
-        }
-        if (ball.right >= positionBoard.right) {
-            if (getPoint('left'))
-                return Do.END;
-        }
-
-        // if the ball touch the left or right paddle
-        if (ball.left <= player.right
-            && ball.bottom >= player.top
-            && ball.top <= player.bottom
-            && ball.left >= player.left) {
-            getNewDirection('left');
-            return Do.CONTINUE;
-        }
-        if (ball.right >= player.left
-            && ball.bottom >= player.top
-            && ball.top <= player.bottom
-            && ball.right <= player.right) {
-            getNewDirection('right');
-            return Do.CONTINUE;
-        }
-
-        // if the ball touch the top or bottom of the board
-        if (ball.top <= positionBoard.top) {
-            ball.directionY = -ball.directionY;
-        }
-        if (ball.bottom >= positionBoard.bottom) {
-            ball.directionY = -ball.directionY;
-        }
-
-        // if the ball the top or bottom of the paddle
-        if ((ball.top <= player.bottom
-            || ball.bottom >= player.top)
-            && ball.left <= player.right
-            && ball.right >= player.left) {
-            ball.directionY = -ball.directionY;
-        }
-        if ((ball.bottom >= player.top
-            || ball.top <= player.bottom)
-            && ball.left <= player.right
-            && ball.right >= player.left) {
-            ball.directionY = -ball.directionY;
-        }
-
-        ball.move();
-    }
-
-    function moveAll() {
-        if (moveBall() === Do.END)
-            return ;
-        movePaddle();
-        setTimeout(moveAll, 10);
-    }
-
-    function resetGame() {
-        console.log('reset');
-        player.score = 0;
-        player.score = 0;
-        player.resetPlace();
-        player.resetPlace();
-        ball.resetPlace();
-    }
-
-    function startGame() {
-        if (gameState === 'notStarted') {
-            gameState = 'started';
-            resetGame();
-            moveAll();
-        }
-    }
 });
 
 
